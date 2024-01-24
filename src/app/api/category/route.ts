8ddf4f49f82f8b785e7b8077be5c4997dce921a6
Hiro Nishi <hiroyuki122003@gmail.com>
2024-01-24 19:40:45 +0900

    modified

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -16,7 +16,7 @@ export async function GET(req: NextRequest) {
       return NextResponse.json({
         success: true,
         data: getBlogPostListBasedOnCurrenrCategoryID,
-      });
+      });   
     } else {
         return NextResponse.json({
             success: false,
